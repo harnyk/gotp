aef77138803f149ba6627271dea415350c4ce80b
Mark <markh@sweepbright.com>
2022-04-23 15:41:10 +0300

    docs

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# Gotp - encrypted TOTP manager
+
+## Usage
+
+    gotp add "My secret"       # Add a new secret
+    gotp list                  # List all secrets, choose one and generate a code
+    gotp remove "My secret"    # Remove a secret
+    gotp generate "My secret"  # Generate a TOTP code for a secret
--- main.go
+++ main.go
@@ -23,41 +23,45 @@ func main() {
 	var rootCmd = &cobra.Command{
 		Use:   "gotp",
 		Short: "A simple tool to generate and store TOTP codes",
-		Long:  `A simple tool to generate and store TOTP codes`,
+		Long:  "Gotp stores your TOTP codes in a local file encrypted with a master password.",
 	}
 
 	rootCmd.AddCommand(
 		&cobra.Command{
-			Use:   "list",
-			Short: "List all secrets",
-			Long:  `List all secrets`,
+			Use:     "list",
+			Short:   "List all secrets",
+			Long:    "List all secrets. Select a secret to generate a code",
+			Aliases: []string{"ls", "l"},
 			Run: func(cmd *cobra.Command, args []string) {
 				app.CmdList()
 			},
 		},
 		&cobra.Command{
-			Use:   "show <key>",
-			Short: "Generate code for a secret",
-			Long:  `Generate code for a secret`,
-			Args:  cobra.ExactArgs(1),
+			Use:     "show <key>",
+			Short:   "Generate code for a secret",
+			Long:    "Generate a OTP code for a secret",
+			Aliases: []string{"generate", "gen", "s"},
+			Args:    cobra.ExactArgs(1),
 			Run: func(cmd *cobra.Command, args []string) {
 				app.CmdGenerate(args[0])
 			},
 		},
 		&cobra.Command{
-			Use:   "add <key>",
-			Short: "Add a secret",
-			Long:  `Add a secret`,
-			Args:  cobra.ExactArgs(1),
+			Use:     "add <key>",
+			Short:   "Add a secret",
+			Long:    "Add a secret. You will be prompted to enter the secret",
+			Aliases: []string{"a", "new", "create"},
+			Args:    cobra.ExactArgs(1),
 			Run: func(cmd *cobra.Command, args []string) {
 				app.CmdAdd(args[0])
 			},
 		},
 		&cobra.Command{
-			Use:   "delete <key>",
-			Short: "Delete a secret",
-			Long:  `Delete a secret`,
-			Args:  cobra.ExactArgs(1),
+			Use:     "delete <key>",
+			Short:   "Delete a secret",
+			Long:    "Delete a secret by key",
+			Aliases: []string{"rm", "remove", "del", "d"},
+			Args:    cobra.ExactArgs(1),
 			Run: func(cmd *cobra.Command, args []string) {
 				fmt.Println("Deleting secret")
 			},
